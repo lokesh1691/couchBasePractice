57062d2013ef0baa414aa5ae1ea26b825311dbb2
unknown <lokeshappsstar@gmail.com>
2021-03-02 22:29:36 +0530

    couchbase demo

---
+++ src/main/java/com/example/couchbaseCRUD/CouchbaseCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.couchbaseCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchbaseCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouchbaseCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/couchbaseCRUD/CouchbaseCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.couchbaseCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouchbaseCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
