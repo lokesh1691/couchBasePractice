158993c819a56c944b365a48b2f3447e2cfca31e
unknown <lokeshappsstar@gmail.com>
2021-03-02 23:04:15 +0530

    save

--- src/main/java/com/example/couchbaseCRUD/CouchbaseCrudApplication.java
+++ src/main/java/com/example/couchbaseCRUD/CouchbaseCrudApplication.java
@@ -1,13 +1,24 @@
 package com.example.couchbaseCRUD;
 
+
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
+
 public class CouchbaseCrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CouchbaseCrudApplication.class, args);
-	}
+	
+
+      public static void main(String[] args) {
+		
+SpringApplication.run(CouchbaseCrudApplication.class, args);
+	
+  }
+
 
 }
